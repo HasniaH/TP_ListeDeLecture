34a889dd5ffadfa6ca05276418a2b7e26b288065
Hassnia <hasnia.deyah@gmail.com>
2021-05-16 21:46:10 -0400

    Creation de la page accueil

--- liste-de-lecture_frontend/src/App.js
+++ liste-de-lecture_frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import  PageAccueil from './Pages/PageAccueil'
+import {BrowserRouter as Router,Link, Route} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+         < Route path="/" component={ PageAccueil} exact/>
+      </Container>
+    </Router>
   );
 }
-
 export default App;
---
+++ liste-de-lecture_frontend/src/Pages/PageAccueil.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ function PageAccueil() {
+    return (
+        <>
+           <h1>Bienvenu</h1> 
+           <p>C'est une site pour les musiciens </p>
+        </>
+    )
+}export default PageAccueil
