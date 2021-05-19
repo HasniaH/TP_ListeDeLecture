d594031646f1d6b3e837c3311b332e9efefa5bd5
ThierryNews <1995159@csfoy.ca>
2021-05-19 09:21:25 -0400

    Ajout de l'identifiant

--- Liste_De_Lecture_bakend/src/server.js
+++ Liste_De_Lecture_bakend/src/server.js
@@ -3,11 +3,11 @@ import express, { request } from 'express';
 const app = express();
 const pieces = {
     'Category1': {
-    'piece1':'p1',
-    'piece2':'p2',
-    'piece3':'p3'
+        'piece1': 'p1',
+        'piece2': 'p2',
+        'piece3': 'p3'
     }
-      
+
 }
 
 
@@ -17,5 +17,7 @@ app.get('/api/pieces', (requete, reponse) => {
 app.get('/api/pieces/TestTest', (requete, reponse) => {
     reponse.status(200).json(pieces);
 });
-
-app.listen(8000,()=>console.log('Ecoute le port 8000'));
+app.get('/api/pieces/:id', (requete, reponse) => {
+    reponse.status(200).json(pieces);
+});
+app.listen(8000, () => console.log('Ecoute le port 8000'));
