2d98ef72ef1047c44e93d75a8f2c1e736ea7dfe9
Hassnia <hasnia.deyah@gmail.com>
2021-05-19 09:31:58 -0400

    Ajout de la route id

--- Liste_De_Lecture_bakend/src/server.js
+++ Liste_De_Lecture_bakend/src/server.js
@@ -7,7 +7,6 @@ const pieces = {
         'piece2': 'p2',
         'piece3': 'p3'
     }
-
 }
 
 
@@ -19,5 +18,14 @@ app.get('/api/pieces/TestTest', (requete, reponse) => {
 });
 app.get('/api/pieces/:id', (requete, reponse) => {
     reponse.status(200).json(pieces);
+});
+app.get('/api/pieces/:id', (requete, reponse) => {
+    const idPiece= requete.params.id;
+    console.log(idPiece);
+    const ObjectId  = require('mongodb').ObjectID;
+    UtiliserDB(async (db) => {
+        const infoPiece = await db.collection('pieces').findOne({_id:ObjectId(idPiece)});
+        reponse.status(200).json(infoPiece);
+    }, reponse)
 });
 app.listen(8000, () => console.log('Ecoute le port 8000'));
