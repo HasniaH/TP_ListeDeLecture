23e50412d62f6ba5eeeaa901b205916cdaf91ca7
ThierryNews <1995159@csfoy.ca>
2021-05-20 09:38:04 -0400

    Modification de la page Acceuil

--- liste-de-lecture_frontend/src/Pages/PageAccueil.js
+++ liste-de-lecture_frontend/src/Pages/PageAccueil.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
- function PageAccueil() {
+function PageAcceuil() {
     return (
         <>
-           <h1>Bienvenu</h1> 
-           <p>C'est une site pour les musiciens </p>
+            <h1>Description</h1>
+            <p>
+                Les musiciens et les chanteurs jouent ou chantent dans des orchestres,
+                des chorales, des compagnies d'opéra et des formations musicales populaires,
+                dans des établissements tels que des salles de concert, des bars-salons et des théâtres,
+                dans des productions cinématographiques, à la télévision et dans des studios d'enregistrement.
+                Ce groupe de base inclut également les professeurs de musique qui enseignent habituellement
+                dans des conservatoires, des académies et des maisons privées.
+            </p>
         </>
     )
-}export default PageAccueil
+} export default PageAcceuil
