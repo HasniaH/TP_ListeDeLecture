8ea26f324e965a537c5e4b997a170d5b6b9b37ad
Hassnia <hasnia.deyah@gmail.com>
2021-05-20 09:30:49 -0400

    Ajout du route Ajouter piece

--- Liste_De_Lecture_bakend/src/server.js
+++ Liste_De_Lecture_bakend/src/server.js
@@ -1,14 +1,20 @@
 import express, { request } from 'express';
-
 const app = express();
-const pieces = {
-    'Category1': {
-        'piece1': 'p1',
-        'piece2': 'p2',
-        'piece3': 'p3'
+app.use(express.json());
+
+import { MongoClient } from 'mongodb';
+
+const UtiliserDB = async (operations, reponse) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true });
+        const db = client.db('Tp_ListeDeLecture');
+        await operations(db);
+        client.close();
+    }
+    catch (erreur) {
+        reponse.status(500).send("Erreur de connection à la  bases de données  ", erreur);
     }
 }
-
 
 app.get('/api/pieces', (requete, reponse) => {
     UtiliserDB(async (db) => {
@@ -25,5 +31,27 @@ app.get('/api/pieces/:id', (requete, reponse) => {
         const infoPiece = await db.collection('pieces').findOne({ _id: ObjectId(idPiece) });
         reponse.status(200).json(infoPiece);
     }, reponse)
+});
+app.post('/api/pieces/ajouter', (requete, reponse) => {
+    const {titre, artiste, categories} = requete.body;
+    console.log(requete.body);
+    if(titre !== undefined && artiste !== undefined && categories !== undefined) {
+        UtiliserDB (async (db) => {    
+          await db.collection('pieces').insertOne({
+            titre: titre,
+            artiste: artiste,
+            categories: categories
+          });
+          reponse.status(200).send('piece ajouté');    
+        } , reponse).catch(
+           () =>reponse.status(500).send("Erreur : la piece n'a pas été ajouté")
+        );
+    }
+    else {
+        reponse.status(500).send(`Certains paramètres ne sont pas définis:
+        - titre: ${titre},
+        - artiste: ${artiste},
+        - categories: ${categories}`);
+    }
 });
 app.listen(8000, () => console.log('Ecoute le port 8000'));
